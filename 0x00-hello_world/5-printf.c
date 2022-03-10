d40200d998beaf1514c5ee630d1c74ef7829e856
root <kenneth4justice@gmail.com>
2022-03-10 13:36:21 -0800

    Add solution for exercise-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,8 @@
  *
  * Return: returns 0 after printing
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
