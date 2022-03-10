3a05477a6b6f3c2d637effc705cfa49c5bbd7f23
root <kenneth4justice@gmail.com>
2022-03-10 14:34:33 -0800

    Initial solution

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print a sentence followed by a new line
+ *
+ * Return: returns 1 after printing
+ */
+int main(void)
+{
+	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n"
