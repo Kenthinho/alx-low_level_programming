28f2b8634842877a1fcaf3d9b54d16a9aca70bc4
Kenneth Onuma <74187326+Kenthinho@users.noreply.github.com>
2022-03-10 23:36:04 +0100

    Add solution for exercise-101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - print a sentence followed by a new line
+ * main - prints a sentence followed by a new line
  *
  * Return: returns 1 after printing
- */
+*/
+
 int main(void)
 {
-	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
