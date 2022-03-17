6208f2442c5a858c2648ec87ae94c6fe40f2b6c1
root <kenneth4justice@gmail.com>
2022-03-17 06:18:16 -0700

    Add solution for exercise-1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - checks the variable c if digit
+ * returns 1 if true otherwise 0
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+if (isdigit(int c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
