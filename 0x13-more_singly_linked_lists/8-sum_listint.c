a01b9c0b63cd32a9a732a50c8b6035b16cee5043
root <kenneth4justice@gmail.com>
2022-04-25 11:17:17 -0700

    Sums up node in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - A function that return sum of all data
+ * @head: pointer to the head
+ * Return: sum in int
+ */
+
+int sum_listint(listint_t *head)
+{
+listint_t *temp = head;
+int sum = 0;
+if (!head)
+return (0);
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
+}
