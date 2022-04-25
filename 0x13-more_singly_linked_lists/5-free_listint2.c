decf244d11c8203e61028b991a0ae2e2ee27e308
root <kenneth4justice@gmail.com>
2022-04-25 11:12:47 -0700

    Frees memory used

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the lisint_t list
+ */
+
+void free_listint(listint_t *head)
+{
+if (!head)
+return;
+if (head->next)
+{
+free_listint(head->next);
+}
+free(head);
+}
+
+/**
+ * free_listint2 - a function that free lists in memory and set
+ * the head to null
+ * @head:parameter head pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+if (!head)
+return;
+if (*head)
+free_listint(*head);
+*head = NULL;
+}
