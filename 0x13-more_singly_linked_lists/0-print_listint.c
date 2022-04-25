a1966a2d88bf1caac26740075654c7413e624020
root <kenneth4justice@gmail.com>
2022-04-25 10:54:12 -0700

    Prints all element

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that lists all elements of the linked list
+ * @h: header pointer param
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t count;
+if (h == NULL)
+return (0);
+count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+count++;
+h = h->next;
+}
+return (count);
+}
