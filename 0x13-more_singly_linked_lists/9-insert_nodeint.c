103453704238723388b3761e16e1844f197f41e6
root <kenneth4justice@gmail.com>
2022-04-26 15:26:38 -0700

    Inserts node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,3 @@
-#include "lists.h"
-
 /**
  * insert_nodeint_at_index - A function that
  * inserts a new node at a given position
@@ -11,32 +9,33 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0;
-listint_t *current_node = *head, *new_node_ptr;
-if (idx == 0)
-{
-new_node_ptr = malloc(sizeof(listint_t));
-if (!new_node_ptr)
-return (NULL);
-new_node_ptr->n = n;
-new_node_ptr->next = *head;
-*head = new_node_ptr;
-return (new_node_ptr);
-}
-while (current_node && ((i + 1) != idx))
-{
-current_node = current_node->next;
-i++;
-}
-if ((i + 1) == idx)
-{
-new_node_ptr = malloc(sizeof(listint_t));
-if (!new_node_ptr)
-return (NULL);
-new_node_ptrr->n = n;
-new_node_ptr->next = current_node->next;
-current_node->next = new_node_ptr;
-return (new_node_ptr);
-}
-return (NULL);
+	listint_t *current;
+	listint_t *newNode;
+
+	register unit i;
+
+	newNode = malloc(sizeof(listint_t));
+	if (!head || !newNode)
+		return (NULL);
+	newNode->n = n;
+	if (!idx)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	current = *head;
+	while (current)
+	{
+		if (i == idx - 1)
+		{
+			newNode->next = current->next;
+			current->next = newNode;
+			return (newNode);
+		}
+		i++;
+		current = current->next;
+	}
+	free(newNode);
+	return (NULL);
 }
