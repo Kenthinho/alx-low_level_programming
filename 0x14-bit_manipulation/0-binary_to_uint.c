5ce1e546ebda0c7860606e4e49f69e5c8d583be2
root <kenneth4justice@gmail.com>
2022-04-28 06:30:41 -0700

    Converts binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,11 @@ unsigned int binary_to_uint(const char *b)
 {
 int i = 0, m = 1;
 unsigned int result = 0;
-
 if (!b)
 return (0);
-
 while (*(b + i) != '\0')
 {
-if (*(b + i) != '\0' && *(b + i) != '1')
+if (*(b + i) != '0' && *(b + i) != '1')
 return (0);
 i++;
 }
