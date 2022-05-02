77c7e8d13e13a575ce5797d8da213879dbec7492
root <kenneth4justice@gmail.com>
2022-05-02 07:18:19 -0700

    Reads text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that read text file and print on stdout
+ * @filename: name of file
+ * @letters: num of letters to be printed
+ * Return: number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t num_of_letters, ftable;
+char *buff;
+if (!filename)
+return (0);
+fd = open(filename, 0_RDONLY);
+if (fd == -1)
+return (0);
+buff = malloc(letters);
+if (!buff)
+return (0);
+ftable = read(fd, buff, letters);
+if (ftable == -1)
+return (0);
+num_of_letters = write(STDOUT_FILENO, buff, ftable);
+if (num_of_letters == -1 || ftable != num_of_letters)
+return (0);
+free(buff);
+close(fd);
+return (num_of_letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+void ensure_endianness(char little_e, void *data, insigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
+
+#endif
