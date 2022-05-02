cd953a82e5fe2c3acc750b404512c7fabeb16fbb
root <kenneth4justice@gmail.com>
2022-05-02 07:37:26 -0700

    Creates new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a file
+ * @filename: name of file
+ * @text_content: NULL terminated string
+ * Return: int 1 on success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int file, i, w_count;
+if (!filename)
+return (-1);
+file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
+if (text_content)
+{
+i = 0;
+while (text_content[i])
+i++;
+w_count = write(file, text_content, i);
+if (w_count == -1)
+return (-1);
+}
+close(file);
+return (1);
+}
