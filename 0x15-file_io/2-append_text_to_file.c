71becd06afc41da4b1df5debf3632ae596034715
root <kenneth4justice@gmail.com>
2022-05-02 17:36:55 -0700

    File descriptions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, len = 0, wstatus;
-if (filename == NULL)
-return (-1);
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
-return (-1);
-if (text_content)
-{
-while (text_content[len] != '\0')
-len++;
-wstatus = write(fd, text_content, len);
-if (wstatus == -1)
-return (-1);
-}
-close(fd);
-return (1);
+	int fd, t, s = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+		if (t != s)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
