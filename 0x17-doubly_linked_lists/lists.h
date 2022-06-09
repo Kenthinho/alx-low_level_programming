83a0f2024f9b0f5e31f798fff207e9f721c5ed8f
root <kenneth4justice@gmail.com>
2022-06-09 15:16:01 -0700

    print all elements inside a doubly linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <string.h>
+
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -12,6 +13,7 @@
  * Description: doubly linked list node structure
  * 
  */
+
 typedef struct dlistint_s
 {
   int n;
@@ -28,4 +30,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
+
 #endif
