6811336705294c23c801127a8f725dc0f16eb76c
Kenneth Onuma <74187326+Kenthinho@users.noreply.github.com>
2022-06-09 22:52:35 +0100

    Fix typo in file naming

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,13 +10,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * 
  */
-
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+  int n;
+  struct dlistint_s *prev;
+  struct dlistint_s *next;
 } dlistint_t;
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
