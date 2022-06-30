6f61051d5718a4f0e48d1fc7263f3c696dbc5fb1
root <kenneth4justice@gmail.com>
2022-06-30 13:39:30 -0700

    create new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+hash_table_t *table;
+unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
+table = malloc(sizeof(hash_table_t));
+if (!table)
+{
+free(table);
+return (NULL);
+}
+table->size = size;
+table->array = malloc(sizeof(hash_table_t *) * size);
+if (!table->array)
+{
+free(table);
+return (NULL);
+}
+while (i < size)
+{
+table->array[i] = NULL;
+i++;
+}
+return (table);
+}
