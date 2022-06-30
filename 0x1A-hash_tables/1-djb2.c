c71425339efd3be35fcea1faeb8166e34a01e005
root <kenneth4justice@gmail.com>
2022-06-30 13:46:54 -0700

    djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,8 +12,9 @@ unsigned long int hash;
 int c;
 
 hash = 5381;
+
 while ((c = *str++))
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash)+c; /* hash * 33 + c */
 
 return (hash);
 }
